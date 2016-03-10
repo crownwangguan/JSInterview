3a6829af26b444c793976a520a0db9b84458ad3b
crownwangguan <crown.wg@outlook.com>
2016-03-09 21:00:05 -0500

    JS part1

--- InterviewPerpare.js
+++ InterviewPerpare.js
@@ -666,14 +666,20 @@ Answer: There are four different ways
 15. Event bubble
 Question: What is event bubble? How does event flows?
 
-	Capture: When you clicked, browser knows a click event occurred. It starts from the window (lowest level/root of your website), 
-			then goes to document, then html root tag, then body, then table... its trying to reach the the as lowest level of element as possible.
+	Capture: When you clicked, browser knows a click event occurred. 
+			It starts from the window (lowest level/root of your website), 
+			then goes to document, then html root tag, then body, then table... 
+			its trying to reach the the as lowest level of element as possible.
 			This is called capture phase (phase -1).
-	Target: When browser reach the lowest level of element. In this case, you have clicked on a table cell (table data) hence target would be "td" tag. 
-			Then browser checks whether you have any click handler attached to this element. If there is any, browser executes that click hander. 
+	Target: When browser reach the lowest level of element. In this case, you have 
+			clicked on a table cell (table data) hence target would be "td" tag. 
+			Then browser checks whether you have any click handler attached to this element. 
+			If there is any, browser executes that click hander. 
 			This is called target phase (phase -2).
-	Bubbling: After firing click hander attached to "td", browser walks toward root. One level upward and check whether there is any click handler 
-			attached with table row ("tr" element). If there is any it will execute that. Then it goes to tbody, table, body, html, document, window. 
+	Bubbling: After firing click hander attached to "td", browser walks toward root. 
+			One level upward and check whether there is any click handler 
+			attached with table row ("tr" element). If there is any it will execute that. 
+			Then it goes to tbody, table, body, html, document, window. 
 			In this stage its moving upward and this is called event bubbling or bubbling phase (phase-3). 
 
 
@@ -747,5 +753,437 @@ Answer: HTML parser will ignore defer and async keyword for inline script (scrip
 	defer: defer keyword in the script tag will defer the execution of the script. Hence script will be executed when DOM is available. 
 			Important point is, defer is not supported by all major major browsers.
 	async: If possible, set the execution of the script, asynchronously. async keyword has no effect on inline script.
+
++++++++++++++++++++++++++++++++++++++++JavaScript+++++++++++++++++++++++++++++++++++++++
+
+
+1. null vs undefined
+Question: What are the differences between null and undefined?
+Answer: JavaScript has two distinct values for nothing, null and undefined.
+
+undefined means, value of the variable is not defined. JavaScript has a global variable undefined 
+		whose value is "undefined" and typeof undefined is also "undefined". Remember, undefined is 
+		not a constant or a keyword. undefined is a type with exactly one value: undefined. Assigning 
+		a new value to it does not change the value of the type undefined.
+
+null means empty or non-existent value which is used by programmers to indicate “no value”. 
+		null is a primitive value and you can assign null to any variable. null is not an object, 
+		it is a primitive value. For example, you cannot add properties to it. Sometimes people 
+		wrongly assume that it is an object, because typeof null returns "object".
+
+Btw, null == undefined
+
+
+2. == Vs ===
+Question: What are the differences between == and ===?
+Answer: The simplest way of saying that, == will not check types and === will check whether both sides are of same type. 
+		So, == is tolerant. But under the hood it converts to its convenient type to have both in same type and then do the comparison.
+
+=== compares the types and values. Hence, if both sides are not same type, answer is always false. 
+
+If both operands are same type use ===
+undefined == null
+If one operands is string another is number, convert string to number
+If one is boolean and another is non-boolean, convert boolean to number and then perform comparison
+While comparing a string or number to an object, try to convert the object to a primitive type and then try to compare
+Be careful while comparing objects, identifiers must reference the same objects or same array.
+
+
+3. Object Equality
+Question: How would you compare two objects in JavaScript?
+Basics: JavaScript has two different approaches for testing equality. Primitives like strings and numbers are compared by their value, 
+		while objects like arrays, dates, and user defined objects are compared by their reference. This means it compares whether 
+		two objects are referring to the same location in memory.
+
+Answer: Equality check will check whether two objects have same value for same property. To check that, you can get the keys for 
+		both the objects. If the number of properties doesnt match, these two objects are not equal. Secondly, you will check each 
+		property whether they have the same value. If all the properties have same value, they are equal
+
+
+function isEqual(a, b) {
+    var aProps = Object.getOwnPropertyNames(a),
+        bProps = Object.getOwnPropertyNames(b);
+
+    if (aProps.length != bProps.length) {
+        return false;
+    }
+
+    for (var i = 0; i < aProps.length; i++) {
+        var propName = aProps[i];
+        
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+4. True Lies
+Question: 11+ true false related questions that will trick you.
+
+falsy: In javascript 6 things are falsy and they are- false, null, undefined, '', 0, NaN
+truthy: There are only two truthy things- true and everything that is not false
+
+
+5. Truthy isnt Equal to true
+Question: As [] is true, []==true should also be true. right?
+
+Answer: You are right about first part, [], empty array is an object and object is always truthy. 
+		Hence, if you use if([]){console.log('its true')} you will see the log.
+
+		However, special case about == (double equal) is that it will do some implicit coercion.
+		Since left and right side of the equality are two different types, JavaScript cant compare 
+		them directly . Hence, under the hood, JavaScript will convert them to compare. 
+		first right side of the equality will be cooereced to a number and number of true would be 1.
+
+		After that, JavaScript implementation will try to convert [] by usingtoPrimitive 
+		(of JavaScript implementation). since [].valueOf is not primitive will use toString and will get "" 
+		Now you are comparing "" == 1 and still left and right is not same type. 
+		Hence left side will be converted again to a number and empty string will be 0.
+
+		Finally, they are of same type, you are comparing 0 === 1 which will be false.
+
+6. Extend Core Object
+Question: How could you write a method on instance of a date which will give you next day?
+Answer: I have to declare a method on the prototype of Date object. To get access to the current 
+		value of the instance of the date, i will use this
+
+		Date.prototype.nextDay = function(){
+		  var currentDate = this.getDate();
+		  return new Date(this.setDate(currentDate +1));
+		}
+
+		var date = new Date(); 
+		date; //Fri May 16 2014 20:47:14 GMT-0500 (Central Daylight Time)
+		date.nextDay();//Sat May 17 2014 20:47:14 GMT-0500 (Central Daylight Time)
+
+
+7. bind
+Question: If you want to use an arbitrary object as value of this, how will you do that?
+Answer: There are at least three different ways to doing this by using bind, call and apply. 
+
+		var monica = {
+		  name: 'Monica Geller',
+		  total: 400,
+		  deductMontlyFee: function(fee){
+		     this.total = this.total - fee;
+		     return this.name + ' remaining balance is '+ this.total; 
+		  }
+		}
+        
+		var rachel = {name: 'Rachel Green', total: 1500};
+		var rachelFeeDeductor = monica.deductMonthlyFee.bind(rachel, 200);
+
+		rachelFeeDeductor(); //"Rachel Green remaining balance is 1300"
+		rachelFeeDeductor(); //"Rachel Green remaining balance is 1100"
+        
+		bind allows you to borrow a method and set the value of this without calling the function. 
+		It simply returns an exact copy of the Function with new value of this. 
+		You can reuse the same Function with new value of this without harming the old one.
+
+
+		var ross = {name:'Ross Geller', total:250};
+		var rossFeeDeductor = monica.deductMonthlyFee.bind(ross, 25);
+		rossFeeDeductor(); //"Ross Geller remaining balance is 225"
+		rossFeeDeductor(); //"Ross Geller remaining balance is 200"
+
+		rachelFeeDeductor(); //"Rachel Green remaining balance is 900"
+        
+Question: If an older browser dont have bind function, how will you shim it
+Answer:
+
+	Function.prototype.bind = Function.prototype.bind || function(context){
+	  var self = this;
+	  return function(){
+	    return self.apply(context, arguments);
+	  };
+	}
+
+
+8. arguments and call
+Question: Write a simple Function to tell whether 2 is passed as parameter or not?
+Basics: arguments is a local variable, available inside all functions that provides a 
+		collection of all the arguments passed to the function. arguments is not an array 
+		rather an array like object. It has length but doesnt have the methods like forEach, 
+		indexOf, etc.
+Answer: First convert arguments to an array by calling slice method on an array and 
+		pass arguments. After that simply use indexOf.
+
+	function isTwoPassed(){
+	  var args = Array.prototype.slice.call(arguments);
+	  return args.indexOf(2) != -1;
+	}
+
+	isTwoPassed(1,4) //false
+	isTowPassed(5,3,1,2) //true
+
+9. apply
+Question: How could you use Math.max to find the max value in an array?
+Answer: Use apply on Math.max and pass the array as apply takes an array of arguments. 
+		Since we are not reading anything from this or using it at all. We can simply 
+		pass null as first parameter.
+
+	function getMax(arr){
+	  return Math.max.apply(null, arr);  
+	}
+Extra: call and apply, both takes the value of this as first parameter. However, call takes a collection of arguments 
+after first parameter whereas apply use an array of arguments as second parameter.
+
+10. this
+Question: What the heck is "this" in JavaScript?
+Answer: At the time of execution of every function, JavaScript engine sets a property 
+		to the Function called "this" which refer to the current execution context. 
+		"this" is always refer to an object and depends on how Function is called. 
+
+11. log prefix
+Question: How could you set a prefix before everything you log? for example, 
+		if you log('my message') it will log: "(app) my message"
+Answer: Just get the arguments, convert it to an array and unshift whatever prefix you 
+		want to set. Finally, use apply to pass all the arguments to console.
+
+
+	function log(){
+	  var args = Array.prototype.slice.call(arguments);
+	  args.unshift('(app)');
+	  console.log.apply(console, args);
+	}
+
+	log('my message'); //(app) my message
+	log('my message', 'your message'); //(app) my message your message 
+
+
+12. Scope and hoisting
+Question: What will you see in the console for the following example?
+	
+	var a = 1; 
+	function b() { 
+	    a = 10; 
+	    return; 
+	    function a() {} 
+	} 
+	b(); 
+	console.log(a);          
+        
+Answer: 1
+
+Explanation:
+
+Function declaration function a(){} is hoisted first and it behaves like 
+		var a = function () {};. Hence in local scope variable a is created.
+If you have two variables with same name (one in global another in local), 
+		local variable always get precedence over global variable.
+		When you set a = 10;, you are setting the local variable a , not the global one. 
+		Hence, the value of global variable remain same and you get, 1 in the log. 
+Extra: If you didnt have a Function named as "a", you will see 10 in the log.
+
+
+13. Closures Inside Loops
+Question: Look at the code below, you have a for loop if you have setTimeout inside it. 
+		If log the loop counter inside setTimeout, what will be logged?
+
+  	for(var i = 0; i < 10; i++) {
+	    setTimeout(function() {
+	      console.log(i);  
+	    }, 10);
+	}
+Answer: The above will not output the numbers 0 through 9, but will simply print the number 10 ten times.
+
+Explanation: The console log is inside the anonymous Function of setTimeout and 
+			setTimeout is executed when current call stack is over. So, the loop finishes 
+			and before setTimeout get the chance to execute. However, anonymous functions keep 
+			a reference to i by creating a closure. Since, the loop is already finished, 
+			the value i has been set to 10. When setTimeout use the value of i by reference, 
+			it gets the value of i as 10. Hence, you see 10 ten times.
+
+Solution: You can fix it by avoiding closure. it will create its own scope and you can pass i to the function. 
+		In that case i will be a local variable (will not refer to i in the closure) 
+		and value of the i in every loop will be preserved.
+
+		for(var i = 0; i < 10; i++) {
+		    setTimeout((function(i) {
+		      console.log(i);
+		    })(i), 10)
+		}
+
+Alternative Solution: Look at the code below and use your brain (if any).
+
+		for(var i = 0; i < 10; i++) {
+		  setTimeout(console.log.bind(console, i), 10);
+		}
+
+14. delete can delete but
+Question: Look at the code below, I have a property in a object 
+		and I am creating a new object where I am setting it to a new value. 
+		If I delete that property what will i get if I try to access that property?
+
+		var myObject = {
+		    price: 20.99,
+		    get_price : function() {
+		        return this.price;
+		    }
+		};
+		var customObject = Object.create(myObject);
+		customObject.price = 19.99;
+
+		delete customObject.price;
+		console.log(customObject.get_price());
+        
+Answer: You will see 20.99
+
+Explanation: When you create object.create(myObject) you are creating new object 
+		where the myObject will be the parent of the newly created object. 
+		Hence the price property will be at the parent.
+
+		When you are assigning some value to customObject.price, you are creating 
+		a new property on the child. This means, when you delete customObject.price 
+		it deletes the price property in the customObject (in the child). However, 
+		when you call the method getprice, first it looks for this.price in the 
+		child since the customObject doesnt have price property, JavaScript executor 
+		walks through the prototype chain towards the parent. Since customObject was 
+		inherited from myObject and myObject has a price property, the get_price method 
+		returns the price from parent. Hence, you are getting 20.99
+
+
+15. Pass by value or by reference
+Question: Does JavaScript pass parameter by value or by reference?
+Answer: Primitive type (string, number, etc.) are passed by value and objects are passed by reference. 
+		If you change a property of the passed object, the change will be affected. However, you assign 
+		a new object to the passed object, the changes will not be reflected.
+
+
+		var num = 10,
+		    name = "Addy Osmani",
+		    obj1 = {
+		      value: "first value"
+		    },
+		    obj2 = {
+		     value: "second value"
+		    },
+		    obj3 = obj2;
+		 
+		function change(num, name, obj1, obj2) {
+		    num = num * 10;
+		    name = "Paul Irish";
+		    obj1 = obj2;
+		    obj2.value = "new value";
+		}
+		 
+		change(num, name, obj1, obj2);
+		 
+		console.log(num); // 10
+		console.log(name);// "Addy Osmani"
+		console.log(obj1.value);//"first value"
+		console.log(obj2.valuee);//"new value"
+		console.log(obj3.valuee);//"new value"    
+
+
+16. memoization
+Question: How could you implement cache to save calculation time for a recursive fibonacci function?
+Answer: You could use poor mans memoization with a global variable. If fibonacci is already calculated 
+		it is served from the global memo array otherwise it is calculated.
+
+	var memo = [];
+	function _fibonacci(n) {
+	   if(memo[n]){    
+	    return memo[n];
+	   }
+	   else if (n < 2){
+	     return 1;
+	   }else{
+	     fibonacci(n-2) + fibonacci(n-1);
+	   }
+	}
+
+17. Cache Function execution
+Question: How could you cache execution of any function?
+Answer: You could have a method where you will pass a Function and it will internally 
+		maintain a cache object where calculated value will be cached. When you will 
+		call the Function with same argument, the cached value will be served.
+
+
+	function cacheFn(fn) {
+	    var cache={};
+	    
+	    return function(arg){
+	        if (cache[arg]){
+	           return cache[arg];
+	        }
+	        else{
+	           cache[arg] = fn(arg);
+	            return cache[arg];
+	        }
+	    }
+	}
+
+18. JQuery style chaining
+Question: If you need to implement the following chaining with call back, 
+		how will you implement it?
+
+	var obj = {   // every method returns obj---------v
+	    first: function() { console.log('first');   return obj; },
+	    second: function() { console.log('second'); return obj; },
+	    third: function() { console.log('third');   return obj; }
+	}
+
+	obj.first().second().third();
+
+19. Animation
+Question: How could you implement moveLeft animation?
+Answer: Use setInterval that will place the element to the left 
+		position by some pixels in every 10ms. Hence, you will see 
+		the element moving towards the desired position. When you call 
+		setInterval, it returns a timeId. After reaching the desired location, 
+		you have to clear the time interval so that Function will not be called 
+		again and again in every 10ms.
+
+
+	function moveLeft(elem, distance) {
+	  var left = 0;
+
+	  function frame() {
+	    left++;
+	    elem.style.left = left + 'px';
+
+	    if (left == distance)
+	      clearInterval(timeId)
+	  }
+
+	  var timeId = setInterval(frame, 10); // draw every 10ms
+	}
+
+
+20. Currying
+Question: How would you implement currying for any functions?
+What is curring: Curring is partial invocation of a function. 
+		Currying means first few arguments of a Function is pre-processed and a 
+		Function is returned. The returning Function can add more arguments 
+		to the curried function. Its like if you have given one or two spice 
+		to the curry and cooked little bit, still you can add further spice to it. 
+		A simple example will look like
+
+
+		function addBase(base){
+		  return function(num){
+		    return base + num;
+		  }
+		}
+
+		var addTen = addBase(10);
+		addTen(5); //15
+		addTen(80); //90
+		addTen(-5); //5
+        
+Explanation: You are creating a closure that return a function. 
+	When you are curring with a new number, new number is added to the base you 
+	have provided.
+
+
+
+
+
+
+
+
+
 
 
